aa45ec13f244a27fbf7293c2b32e8410ca619ec0
CalvinDering <calvin.dering@gmail.com>
2024-07-08 04:14:08 +0200

    Added car suspension

---
+++ LowPolyRacing/Assets/_Scripts/CarController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    [Header("References")]
+    [SerializeField] private Rigidbody carRB;
+    [SerializeField] private Transform[] rayPoints;
+    [SerializeField] private LayerMask drivable;
+
+    [Header("Suspension Settings")]
+    [SerializeField] private float springStiffness;
+    [SerializeField] private float damperStiffness;
+    [SerializeField] private float restLength;
+    [SerializeField] private float springTravel;
+    [SerializeField] private float wheelRadius;
+
+    private void Start() {
+        carRB = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        Suspension();
+    }
+
+    private void Suspension() {
+        foreach(Transform rayPoint in rayPoints) {
+            RaycastHit hit;
+            float maxLength = restLength + springTravel;
+
+            if(Physics.Raycast(rayPoint.position, -rayPoint.up, out hit, maxLength + wheelRadius, drivable)) {
+                float currentSpringLength = hit.distance - wheelRadius;
+                float springCompression = restLength - currentSpringLength / springTravel;
+
+                float springVelocity = Vector3.Dot(carRB.GetPointVelocity(rayPoint.position), rayPoint.up);
+                float dampForce = damperStiffness * springVelocity;
+
+                float springForce = springStiffness * springCompression;
+
+                float netForce = springForce - dampForce;
+
+                carRB.AddForceAtPosition(netForce * rayPoint.up, rayPoint.position);
+
+                Debug.DrawLine(rayPoint.position, hit.point, Color.red);
+            } else {
+                Debug.DrawLine(rayPoint.position, rayPoint.position + (wheelRadius + maxLength) * -rayPoint.up, Color.green);
+            }
+        }
+    }
+}
