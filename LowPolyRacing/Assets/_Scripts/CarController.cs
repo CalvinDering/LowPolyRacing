688bc0d285f53066097a54c7c4da578908e2e775
CalvinDering <calvin.dering@gmail.com>
2024-07-08 04:38:47 +0200

    Added car controls

--- LowPolyRacing/Assets/_Scripts/CarController.cs
+++ LowPolyRacing/Assets/_Scripts/CarController.cs
@@ -8,6 +8,7 @@ public class CarController : MonoBehaviour {
     [SerializeField] private Rigidbody carRB;
     [SerializeField] private Transform[] rayPoints;
     [SerializeField] private LayerMask drivable;
+    [SerializeField] private Transform accelerationPoint;
 
     [Header("Suspension Settings")]
     [SerializeField] private float springStiffness;
@@ -16,35 +17,119 @@ public class CarController : MonoBehaviour {
     [SerializeField] private float springTravel;
     [SerializeField] private float wheelRadius;
 
+    private int[] wheelsIsGrounded = new int[4];
+    private bool isGrounded = false;
+
+    [Header("Input")]
+    private float moveInput = 0;
+    private float steerInput = 0;
+
+    [Header("Car Settings")]
+    [SerializeField] private float acceleration = 25f;
+    [SerializeField] private float maxSpeed = 100f;
+    [SerializeField] private float deceleration = 10f;
+    [SerializeField] private float steerStrength = 15f;
+    [SerializeField] private AnimationCurve turningCurve;
+    [SerializeField] private float dragCoefficient = 1f;
+
+    private Vector3 currectCarLocalVelocity = Vector3.zero;
+    private float carVelocityRatio = 0;
+
     private void Start() {
         carRB = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate() {
         Suspension();
+        GroundCheck();
+        CalculateCarVelocity();
+        Movement();
+    }
+
+    private void Update() {
+        GetPlayerInput();
+    }
+
+    private void Movement() {
+        if(isGrounded) {
+            Acceleration();
+            Deceleration();
+            Turn();
+            SidewaysDrag();
+        }
+    }
+
+    private void Acceleration() {
+        carRB.AddForceAtPosition(acceleration * moveInput * transform.forward, accelerationPoint.position, ForceMode.Acceleration);
+    }
+
+    private void Deceleration() {
+        carRB.AddForceAtPosition(deceleration * moveInput * -transform.forward, accelerationPoint.position, ForceMode.Acceleration);
+    }
+
+    private void Turn() {
+        carRB.AddTorque(steerStrength * steerInput * turningCurve.Evaluate(carVelocityRatio) * Mathf.Sign(carVelocityRatio) * transform.up, ForceMode.Acceleration);
+    }
+
+    private void SidewaysDrag() {
+        float currentSidewaysSpeed = currectCarLocalVelocity.x;
+
+        float dragMagnitude = -currentSidewaysSpeed * dragCoefficient;
+
+        Vector3 dragForce = transform.right * dragMagnitude;
+
+        carRB.AddForceAtPosition(dragForce, carRB.worldCenterOfMass, ForceMode.Acceleration);
+    }
+
+    private void GetPlayerInput() {
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+    }
+
+    private void GroundCheck() {
+        int tempGroundedWheels = 0;
+
+        for(int i = 0; i < wheelsIsGrounded.Length; i++) {
+            tempGroundedWheels += wheelsIsGrounded[i];
+        }
+
+        if(tempGroundedWheels > 1) {
+            isGrounded = true;
+        } else {
+            isGrounded = false;
+        }
+    }
+
+    private void CalculateCarVelocity() {
+        currectCarLocalVelocity = transform.InverseTransformDirection(carRB.velocity);
+        carVelocityRatio = currectCarLocalVelocity.z / maxSpeed;
     }
 
     private void Suspension() {
-        foreach(Transform rayPoint in rayPoints) {
+        for(int i = 0; i < rayPoints.Length; i++) {
             RaycastHit hit;
             float maxLength = restLength + springTravel;
 
-            if(Physics.Raycast(rayPoint.position, -rayPoint.up, out hit, maxLength + wheelRadius, drivable)) {
+            if(Physics.Raycast(rayPoints[i].position, -rayPoints[i].up, out hit, maxLength + wheelRadius, drivable)) {
+                wheelsIsGrounded[i] = 1;
+
                 float currentSpringLength = hit.distance - wheelRadius;
                 float springCompression = restLength - currentSpringLength / springTravel;
 
-                float springVelocity = Vector3.Dot(carRB.GetPointVelocity(rayPoint.position), rayPoint.up);
+                float springVelocity = Vector3.Dot(carRB.GetPointVelocity(rayPoints[i].position), rayPoints[i].up);
                 float dampForce = damperStiffness * springVelocity;
 
                 float springForce = springStiffness * springCompression;
 
                 float netForce = springForce - dampForce;
 
-                carRB.AddForceAtPosition(netForce * rayPoint.up, rayPoint.position);
+                carRB.AddForceAtPosition(netForce * rayPoints[i].up, rayPoints[i].position);
 
-                Debug.DrawLine(rayPoint.position, hit.point, Color.red);
+                Debug.DrawLine(rayPoints[i].position, hit.point, Color.red);
             } else {
-                Debug.DrawLine(rayPoint.position, rayPoint.position + (wheelRadius + maxLength) * -rayPoint.up, Color.green);
+                wheelsIsGrounded[i] = 0;
+
+                Debug.DrawLine(rayPoints[i].position, rayPoints[i].position + (wheelRadius + maxLength) * -rayPoints[i].up, Color.green);
             }
         }
     }
