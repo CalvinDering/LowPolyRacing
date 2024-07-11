102befc4b3479603a65eb6d2ea836c7a502ed18a
CalvinDering <calvin.dering@gmail.com>
2024-07-11 05:57:16 +0200

    Added AI avoiding other drivers

--- LowPolyRacing/Assets/_Scripts/CarAIHandler.cs
+++ LowPolyRacing/Assets/_Scripts/CarAIHandler.cs
@@ -13,9 +13,19 @@ public class CarAIHandler : MonoBehaviour {
     [Header("AI Settings")]
     public AIMode aiMode;
     public float maxSpeed = 100;
+    public float detectionRadius = 15f;
+    public float maxDistance = 15f;
+    public float waypointInfluence = 50f;
+    public bool isAvoidingCars = true;
+    public float jitterResponsiveness = 4f;
+    public LayerMask layerMask;
 
     private Vector3 targetPosition = Vector3.zero;
     private Transform targetTransform = null;
+
+    private Vector3 avoidanceVectorLerped = Vector3.zero;
+
+    private Collider[] colliders;
 
     private WaypointNode currentWaypoint = null;
     private WaypointNode[] allWaypoints;
@@ -25,6 +35,7 @@ public class CarAIHandler : MonoBehaviour {
     private void Awake() {
         controller = GetComponent<CarController>();
         allWaypoints = FindObjectsOfType<WaypointNode>();
+        colliders = GetComponentsInChildren<Collider>();
     }
 
     private void FixedUpdate() {
@@ -94,6 +105,10 @@ public class CarAIHandler : MonoBehaviour {
 
         Vector3 vectorToTarget = targetPosition - transform.position;
 
+        if(isAvoidingCars) {
+            AvoidCars(vectorToTarget, out vectorToTarget);
+        }
+
         float angleToTarget = Vector3.SignedAngle(vectorToTarget, transform.forward, Vector3.up);
         angleToTarget *= -1;
 
@@ -101,6 +116,58 @@ public class CarAIHandler : MonoBehaviour {
         steerAmount = Mathf.Clamp(steerAmount, -1.0f, 1.0f);
 
         return steerAmount;
+    }
+
+    private void AvoidCars(Vector3 vectorToTarget, out Vector3 newVectorToTarget) {
+        if(IsCarInFrontOfCar(out Vector3 otherCarPosition, out Vector3 otherCarForwardsVector)) {
+            Vector3 avoidanceVector = Vector3.zero;
+
+            Debug.DrawLine(transform.position, otherCarPosition, Color.cyan);
+
+            avoidanceVector = Vector3.Reflect((otherCarPosition - transform.position).normalized, otherCarPosition + otherCarForwardsVector);
+
+            float distanceToTarget = vectorToTarget.magnitude;
+            float driveToTargetInfluence = waypointInfluence / distanceToTarget;
+
+            driveToTargetInfluence = Mathf.Clamp(driveToTargetInfluence, 0.3f, 1.0f);
+            float avoidanceInfluence = 1.0f - driveToTargetInfluence;
+
+            avoidanceVectorLerped = Vector3.Lerp(avoidanceVectorLerped, avoidanceVector, Time.fixedDeltaTime * jitterResponsiveness);
+
+            newVectorToTarget = vectorToTarget * driveToTargetInfluence + avoidanceVectorLerped * avoidanceInfluence;
+            newVectorToTarget.Normalize();
+
+            Debug.DrawRay(transform.position, avoidanceVector * 10, Color.yellow);
+
+            Debug.DrawRay(transform.position, newVectorToTarget * 10, Color.green);
+
+            return;
+        }
+
+        newVectorToTarget = vectorToTarget;
+    }
+
+    private bool IsCarInFrontOfCar(out Vector3 position, out Vector3 otherCarForwardVector) {
+        RaycastHit hit;
+        Physics.SphereCast(transform.position, detectionRadius, transform.forward, out hit, maxDistance, layerMask);
+
+        if(hit.collider != null && !colliders.Contains(hit.collider)) {
+            Debug.DrawRay(transform.position, transform.forward * maxDistance, Color.red);
+            position = hit.collider.transform.position;
+            otherCarForwardVector = hit.collider.transform.forward;
+            Debug.DrawLine(position, position + otherCarForwardVector * 10, Color.magenta);
+
+            return true;
+        }
+
+        position = Vector3.zero;
+        otherCarForwardVector = Vector3.zero;
+
+        return false;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.position + transform.forward * maxDistance, detectionRadius);
     }
 
 }
